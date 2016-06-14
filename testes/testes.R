2f5314d18b08ea01e20618ae67c84169fd4ca96c
nicolassoarespinto <nicolassoarespinto@hotmail.com>
2016-06-14 18:28:16 -0300

    IMPORTANT
    Delete folder information in line 6. Besides, CensoEducacaoSuperior is smaller and the test would be faster as an example.

--- testes/testes.R
+++ testes/testes.R
@@ -3,13 +3,12 @@ devtools::load_all()
 library(microdadosBrasil)
 
 source('~/microdadosBrasil/testes/teste_functions.R', echo=TRUE)
-folder<- "C:/Users/b2826073/Documents/Testes"
+folder<- "PATH HERE"
 
 
 #Carefull! Function test_download() will delete all files previous inside 'folder'
 
-#teste_download_censo_escolar<- test_download("CensoEscolar", folder,1995:1996)
-teste_read_censo_escolar<- test_read("CensoEscolar",folder,1995:1996)
+#Test for Censo da Educacao Superior
 
-
-t<-read_CensoEscolar("escola", 1995, root_path = folder)
+teste_download_censo_educacao_superior<- test_download("CensoEducacaoSuperior", folder,1995:1996)
+teste<- teste_read_censo_educacao_superior<- test_read("CensoEducacaoSuperior",folder,1995:1996)
