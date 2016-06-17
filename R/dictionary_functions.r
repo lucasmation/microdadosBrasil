73aa0402a2d4e20d209238ab9f5cf4676978980c
nicolassoarespinto <nicolassoarespinto@hotmail.com>
2016-06-17 16:29:18 -0300

    add functions used in reading SAS dictionaries and creating the lists 'Datasets_dics.rda'

--- R/dictionary_functions.r
+++ R/dictionary_functions.r
@@ -1,5 +1,5 @@
 #' @export
-import_SASdictionary <- function(dataset, i, ft){
+get_dictionary <- function(dataset, i, ft){
   x<- environment()
 
   datasets_list<- list.files(system.file("data",package  = "microdadosBrasil"),pattern = "\\.rda") %>%
@@ -20,4 +20,123 @@ import_SASdictionary <- function(dataset, i, ft){
 
   return(dics[[as.character(i)]][[paste0("dic_",ft,"_",i)]])
 }
+
+parses_SAS_import_dic <- function(file){
+  dic_sas   <- readLines(file) %>% sapply(FUN = remove_comments) %>% sapply(str_trim) %>% as.data.frame(stringsAsFactors = FALSE)
+
+
+  names(dic_sas) <- 'a'
+  dic_sas %>% filter(grepl("^@",a)) %>%
+    extract(a, into=c('int_pos', 'var_name', 'x', 'label'),
+            "[[:punct:]\\s+](\\d+)\\s+(\\S+)(?:\\s+([[:graph:]$]+)())?")  %>%
+    mutate(int_pos=int_pos %>% as.numeric,
+           length=gregexpr("[[:digit:]]+(?=\\.)",x,perl = TRUE) %>% regmatches(x = x) %>% as.numeric,
+           decimal_places=gregexpr("(?<=\\.)[[:digit:]]+",x,perl = TRUE) %>% regmatches(x = x) %>% as.numeric) -> dic
+  dic %>% mutate(
+    decimal_places=ifelse(is.na(decimal_places),0,decimal_places),
+    fin_pos= int_pos+length -1,
+    col_type=ifelse(is.na(x),'c',
+                    ifelse(grepl("\\$",x),'c',
+                           ifelse(length<=9 & decimal_places==0,'i','d'))) ,
+    CHAR=ifelse(grepl("\\$",x),TRUE,FALSE)
+  ) -> dic
+
+  #ALGUNS DICIONARIOS N?O MOSTRAM O TAMANHO PARA ALGUNS CAMPOS, APENAS A POSI??O INICIAL E FINAL
+  estimated_length<- dic$int_pos %>% diff %>% c(0)
+  dic$length[is.na(dic$length)]<- estimated_length
+  estimated_final<- dic$int_pos + dic$length
+  dic$fin_pos[is.na(dic$fin_pos)]<- estimated_final[is.na(dic$fin_pos)]
+
+  dic %>% return
+}
+
+get_all_dics<- function(metadata){
+  #facilita o 'parse' de muitos dicionários ao mesmo tempo antes de coloca-los em uma lista
+  #Funciona apenas para diretorios 'bem comportados' como Censo Escolar e PNAD Continua
+
+  for(i in metadata$period){
+
+    if(metadata[metadata$period == i,"format"]== "fwf"){
+      print(i)
+      dics_path <- paste0(ifelse(is.na(metadata[metadata$period==i,'path']),"",metadata[metadata$period==i,'path']),
+                          ifelse(is.na(metadata[metadata$period==i,'path']),"","/"),
+                          ifelse(is.na(metadata[metadata$period==i,'inputs_folder']),"",metadata[metadata$period==i,'inputs_folder']))
+
+      #normal file types (ft)
+      ft_list <- names(metadata)[grepl(names(metadata) ,pattern = "^ft_")] %>% gsub(pattern = "^ft_",replacement ="")
+      for(ft in ft_list){
+        f <- unlist(strsplit(metadata[metadata$period==i,paste0("ft_",ft)], split='&'))[1]
+        if(!is.na(f)){
+          print(paste0(dics_path,'/',f))
+          try({assign(paste0('dic_',ft,"_",i),
+                      parses_SAS_import_dic(paste0(dics_path,ifelse(dics_path == "","",'/'),f)) ,  envir = .GlobalEnv)})
+
+        }
+      }
+
+    }
+
+  }
+}
+
+
+remove_comments<- function(data){
+  data %>% gsub(pattern = "^\\s*/\\*.+?\\*/\\s*$" , replacement = "") %>%
+    gsub(pattern = "/\\*.+?\\*/" , replacement = "") %>%
+    gsub(pattern = ".+?\\*/" , replacement = "") %>%
+    gsub(pattern = "/\\*.+$" , replacement = "") %>%
+
+    #A parte comentada removeria coment?rios em todo o arquivo, como os coment?rios est?o sendo usados como "labels" +
+    # Est?o sendo removidos apenas aqueles que ocupam uma linha inteira
+    return
+}
+
+
+get_individual_dic<- function(ft,i,metadata, dics_path){
+  #function to locate dictionaries in workspace, used to store all dictionaries in a list
+
+  f <- unlist(strsplit(metadata[metadata$period==i,paste0("ft_",ft)], split='&'))[1]
+  if(!is.na(f)){
+    print(paste0(dics_path,'/',f))
+    try({assign(paste0('dic_',ft,"_",i),
+                parses_SAS_import_dic(paste0(dics_path,ifelse(dics_path == "","",'/'),f)))})
+    return(get(paste0('dic_',ft,"_",i)))
+
+  }else{
+    return(NA)
+  }
+}
+
+get_period_dics<- function(i, metadata){
+  #function to locate dictionaries in workspace, used to store all dictionaries in a list
+
+  if(metadata[metadata$period == i,"format"]== "fwf"){
+    print(i)
+    dics_path <- paste0(ifelse(is.na(metadata[metadata$period==i,'path']),"",metadata[metadata$period==i,'path']),
+                        ifelse(is.na(metadata[metadata$period==i,'path']),"","/"),
+                        ifelse(is.na(metadata[metadata$period==i,'inputs_folder']),"",metadata[metadata$period==i,'inputs_folder']))
+
+    #normal file types (ft)
+    ft_list <- names(metadata)[grepl(names(metadata) ,pattern = "^ft_")] %>% gsub(pattern = "^ft_",replacement ="")
+
+    #period_dics<- alply(ft_list,.margins = 1, get_individual_dic2)
+    period_dics<- mapply(FUN = get_individual_dic, ft_list,MoreArgs = list(i = i, metadata = metadata, dics_path = dics_path),SIMPLIFY = FALSE)
+    names(period_dics)<-  paste0("dic_",ft_list,"_",i)
+    return(period_dics)
+  }  else{
+    return(NA)
+  }
+
+}
+
+
+#Creation of Dataset_dics.rda example
+# setwd(path)
+# metadatas<- read_metadata("PNADContinua")
+# get_dics(metadata)
+#
+# Dataset_dics<- mapply(FUN = get_period_dics, metadata$period, MoreArgs = list(metadata = metadata),SIMPLIFY = FALSE)
+# names(Dataset_dics)<- metadata$period
+
+
 
