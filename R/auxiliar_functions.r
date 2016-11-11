bafe972ee5d1b56a9e1901594863ebf2660ff5d3
B2826073 <nicolas.pinto@ipea.gov.br>
2016-11-11 15:23:58 -0200

    add script with test used to check overlapping dictionaries

--- R/auxiliar_functions.r
+++ R/auxiliar_functions.r
@@ -1,9 +1,3 @@
-check_overlapping <- function(dic){
-
-  check = any(dic$int_pos[-1] <= dic$fin_pos[-dim(dic)[1]])
-
-  return(check)
-}
 
 
 
@@ -73,31 +67,4 @@ get_available_filetypes<- function(dataset, period){
 }
 
 
-c <-  data.frame()
 
-for(dataset in get_available_datasets()){
-    for(i in get_available_periods(dataset, fwfonly = TRUE)){
-      for(ft in get_available_filetypes(dataset,i))
-
-
-       print(i)
-       print(ft)
-      d<- data.frame(
-                 `dataset` = dataset,
-                 `i` = i,
-                 `ft` = ft,
-                 overlapping = check_overlapping(get_import_dictionary(dataset,i,ft)), stringsAsFactors = FALSE)
-
-      d[,]<- lapply(d[,], as.character)
-
-      c<- bind_rows(c, d
-      )
-
-    }
-
-
-}
-
-
-c$overlapping %>% as.logical() %>% as.numeric %>% sum()
-(filter(c, overlapping == "TRUE"))
