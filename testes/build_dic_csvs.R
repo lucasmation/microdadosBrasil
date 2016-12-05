e8b8c580b5cefd6b6d0fcbb7e787e4f82ed65785
Nicolas Soares Pinto <B2826073@ipea.gov.br>
2016-12-05 17:58:02 -0200

    script for structure change

---
+++ testes/build_dic_csvs.R
@@ -0,0 +1,58 @@
+for (dataset in c("CadUnico")){
+
+  print(dataset)
+
+  dir.create(paste0("inst/extdata/",dataset))
+  dir.create(paste0("inst/extdata/",dataset, "/dictionaries"))
+
+  #md<- read_metadata(dataset)
+
+ # write.table()
+
+  for(period in get_available_periods(dataset,fwfonly = TRUE)){
+
+    print(period)
+
+    for(ft in get_available_filetypes(dataset,period)){
+
+       print(ft)
+
+      dic<- get_import_dictionary(dataset,period,ft)
+
+
+
+     file = paste0("inst/extdata/",
+                     dataset ,
+                     "/dictionaries/import_dictionary_",
+                     dataset,
+                     "_",
+                     ft,"_",
+                     period, ".csv")
+
+    write.table(x = dic,file = file,
+                  row.names = FALSE, sep = ";")
+
+      #unlink(x  = paste0("inst/extdata/dictionaries/",dataset,"_", ft,"_",period,"_dictionary.csv"))
+
+    }
+  }
+
+}
+
+# for (dataset in get_available_datasets()){
+#   print(dataset)
+#   for(period in get_available_periods(dataset,fwfonly = TRUE)){
+#     print(period)
+#     for(ft in get_available_filetypes(dataset,period)){
+#       print(ft)
+#       dic<- get_import_dictionary(dataset,period,ft)
+#
+#
+#
+#       #write.table(dic, file = paste0("inst/extdata/dictionaries/",dataset,"_", ft,"_",period,"_dictionary.csv"),row.names = FALSE)
+#       unlink(x  = paste0("inst/extdata/dictionaries/",dataset,"_", ft,"_",period,"_dictionary.csv"))
+#
+#     }
+#   }
+#
+# }
