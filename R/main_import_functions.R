dbc058a30f5166fd80efad6d6c87518b2831a6ad
Nicolas Soares Pinto <B2826073@ipea.gov.br>
2017-06-27 16:21:13 -0300

    fix problem in decimal digits #121

--- R/main_import_functions.R
+++ R/main_import_functions.R
@@ -154,7 +154,7 @@ read_data <- function(dataset,ft,i, metadata = NULL,var_translator=NULL,root_pat
     if(any(dic$decimal_places) & dataset == "CENSO"){
       sapply(which(as.logical(dic$decimal_places)), function(x){
         if(dic$col_type[x] == "d"){
-          d[, (x):= d[, x, with = F]/(10**dic$decimal_places[x])]
+          d[, (x):= (lapply(d[, x, with = F], as.numeric) %>% unlist) /(10**dic$decimal_places[x])]
         }
       })
     }
